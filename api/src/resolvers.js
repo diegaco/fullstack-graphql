b7037c8158d79f0833658ff637568bb80c23ec9b
diegaco <diegoacosta211@gmail.com>
2020-03-22 14:27:59 -0300

    creating queries and resolvers, adding input types

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,27 @@
 
 module.exports = {
   Query: {
-    
+    pet(_, {input}, ctx, info) {
+      return ctx.models.Pet.findOne(input)
+    },
+    pets(_, {input}, ctx, info) {
+      return ctx.models.Pet.findMany(input)
+    },
+    user(_, args, ctx, info) {
+      return ctx.models.User.findOne()
+    },
   },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
+  // Mutation: {
+
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+
+  // }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,29 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Pet {
+    id: ID!
+    name: String!
+    createAt: String!
+    type: String
+  }
+
+  input PetInput {
+    name: String
+    type: String
+  }
+
+  type Query {
+    user(id: ID!): User
+    pet(input: PetInput): Pet
+    pets(input: PetInput): [Pet]!
+    users: [User]!
+  }
 
 `;
 
